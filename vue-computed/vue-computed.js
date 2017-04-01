cadbcf446173d934f58a1c458aed6729d6f8251d
sun.yue <sun.yue@chiefclouds.com>
2017-04-01 19:15:32 +0800

    dev

--- vue-computed/vue-computed.js
+++ vue-computed/vue-computed.js
@@ -1,3 +1,78 @@
-/**
- * Created by L.Lawliet-Yue on 2017/3/21.
- */
+
+const Dep = {
+    target: null
+}
+
+function defineReactive(obj, key, val){
+    const deps = []
+
+    Object.defineProperty(obj, key, {
+        get: function(){
+            console.log('set age')
+            if(Dep.target && deps.indexOf(Dep.target) == -1){
+                deps.push(Dep.target)
+            }
+            return val
+        },
+        set: function(newVal){
+            console.log(deps)
+            val = newVal
+            // 通知所有 依赖器 执行计算函数: onDependencyUpdate
+            deps.forEach( updateCallBack => {
+                updateCallBack()
+            })
+        }
+    })
+}
+
+
+function defineComputed(obj, key, computedFn, updateCallBack){
+    const onDependencyUpdate = () => {
+        const val = computedFn()
+        updateCallBack(val)
+    }
+
+    Object.defineProperty(obj, key, {
+        get: function(){
+            console.log(111)
+            Dep.target = onDependencyUpdate
+            const newVal = computedFn()
+            // reset target  其他属性就不会再添加这个依赖
+            Dep.target = null
+            return newVal
+        },
+        set: function(){
+            // nothing 不做 setter
+        }
+    })
+}
+
+
+const person = {}
+
+defineReactive(person, 'name', 'tink')
+defineReactive(person, 'age', 20)
+
+
+defineComputed(
+    person,
+    'status',
+    function(){
+        if(person.age > 18){
+            console.log('this is man')
+            return 'man'
+        }else {
+            console.log('this is boy')
+            return 'boy'
+        }
+    },
+    function(newVal){
+        console.log('person.status now is ' + newVal)
+    }
+)
+
+
+// console.log(person.status)
+person.age = 15
+console.log(person.status)
+person.age = 20
